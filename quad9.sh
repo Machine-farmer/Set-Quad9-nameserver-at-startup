09ceb7dbf8b2174ec3ea34b28caf8f6c620e72fb
Ikram Hossain <ikramhossain.sust@gmail.com>
2023-12-06 09:15:29 +0600

    Updated script to use regex for IP address substitution

--- quad9.sh
+++ quad9.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
 # Check if the user is root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-
 # Use sed to replace the nameserver
-sed -i 's/nameserver 127.0.0.53/nameserver 9.9.9.9/g' /etc/resolv.conf
+sed -i -r 's/nameserver ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/nameserver 9.9.9.9/g' /etc/resolv.conf
